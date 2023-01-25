72833431f75891b22f5c2aa9a48f89b25a15c42d
Moufid Ali <you@example.com>
2023-01-25 11:08:25 +0100

    level 3 add a hashing of the password

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 const { MongoNetworkError } = require("mongodb");
+const md5 = require("md5");
 
 
 const app = express();
@@ -56,7 +57,7 @@ app.get("/secrets",function (req,res) {
 app.post("/register" , function (req,res) {
     const newUser = new User({
         email : req.body.username,
-        password : req.body.password
+        password : md5(req.body.password)
     })
     newUser.save(function (err) {
         if (!err) {
@@ -69,7 +70,7 @@ app.post("/register" , function (req,res) {
 
 app.post("/login" , function (req,res) {
     const userName = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email :userName }, function (err,fonduser) {
         if (!err) {
             if (fonduser.password == password) {
